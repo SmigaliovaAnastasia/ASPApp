2ac6f6df4e66ee1a0c7aad0ada0eeb2e3d9cc018
SmigaliovaAnastasia <angesie6151@gmail.com>
2022-11-21 18:35:35 +0200

    Adding rating to game mapping profile

--- Common/Models/Pagination/Filters/ReviewFilter.cs
+++ Common/Models/Pagination/Filters/ReviewFilter.cs
@@ -9,6 +9,7 @@ namespace ASPApp.Common.Models.Pagination.Filters
             "game_id" => query.Where(r => r.GameId.ToString() == Value),
             "name" => query.Where(r => r.ApplicationUser.UserName.ToLower().Contains(Value.ToLower())),
             "application_user_id" => query.Where(r => r.ApplicationUserId.ToString() == Value),
+            "!application_user_id" => query.Where(r => r.ApplicationUserId.ToString() != Value),
             _ => throw new ArgumentException(),
         };
     }
